2883fd016cd1b0b8e5d5488b0fb4da4699aab20c
Amir-anwar <30289624+Amir-anwar@users.noreply.github.com>
2017-12-02 22:45:21 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+    "use strict";
+
+    // Nav links
+    $(".navbar ul li").on("click", function () {
+        $(this).addClass("active").siblings().removeClass("active");    
+    });
+
+    // Adjust Slider Height
+    var winH    = $(window).height(),
+        upperH  = $(".upper-bar").innerHeight(),
+        navH    = $(".navbar").innerHeight();
+    $(".slider, .carousel-item").height(winH - ( upperH + navH ));
+    
+    // Featured Work Shuffle
+    $(".featured-work ul li").on("click", function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        if ($(this).data("class") === "all") {
+            $(".shuffle-images .col-md").css("opacity", 1)
+        } else {
+            $(".shuffle-images .col-md").css("opacity", ".08")
+            $($(this).data("class")).parent().css("opacity", 1)
+        }       
+    });
+
+});
